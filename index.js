2108038015e83ab962d97cf8984ee2ae571aa2a0
Lucas Nardon <lucasnardon@Lucass-MacBook-Pro.local>
2020-05-18 16:47:39 -0300

    Started Base Website Structure

---
+++ index.js
@@ -0,0 +1,36 @@
+var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        
+  ['blockquote', 'code-block'],
+  [{ 'header': 1 }, { 'header': 2 }],               
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      
+  [{ 'indent': '-2'}, { 'indent': '+2' }],          
+  [{ 'direction': 'rtl' }],                         
+  [{ 'size': ['small', false, 'large', 'huge'] }],  
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  [{ 'color': [] }, { 'background': [] }],
+  [{ 'align': [] }],
+  ['link', 'image'],
+];
+
+var quill = new Quill('#editor', {
+  theme: 'snow',
+  modules: {
+    toolbar: toolbarOptions
+  }
+});
+
+var n = true;
+
+function toggleNavbar () {
+  n = !n;
+  if (n) {
+    document.getElementById("navbar").style.display = 'none';
+    document.getElementById('editor').style.setProperty('--main-color', '#F7F7F7')
+  } else {
+    document.getElementById("navbar").style.display = 'flex';
+  }
+}
+
+// const mainColor = getComputedStyle(document.getElementById('editor')).getPropertyValue('--main-color')
+
